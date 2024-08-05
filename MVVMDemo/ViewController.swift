2a4419d7fcbdc266802676ae23daf0201f3c0843
ya.wang <ya.wang@okg.com>
2024-08-05 16:43:02 +0800

    init

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Wang Ya on 5/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
