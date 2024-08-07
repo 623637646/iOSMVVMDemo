3d16d1c3a39ab6bcbd344dea1acf60f2d5bc99b6
ya.wang <ya.wang@okg.com>
2024-08-07 14:03:32 +0800

    Improve LoginView

--- MVVMDemo/Features/Login/LoginView.swift
+++ MVVMDemo/Features/Login/LoginView.swift
@@ -15,16 +15,47 @@ typealias LoginViewOutputEvent = LoginViewModel.InputFromViewEvent
 class LoginView: BaseView<LoginViewInputEvent, LoginViewOutputEvent> {
     
     // Subviews. We suggest making the subviews private. Only public the needed API to update subviews.
-    private let usernameTextField = UITextField()
-    private let passwordTextField = UITextField()
-    private let loginButton = UIButton(type: .system)
-    private let loadingView = UIActivityIndicatorView()
-    private let tipsView = UILabel()
+    private lazy var usernameTextField: UITextField = {
+        let view = UITextField()
+        view.placeholder = "Username"
+        view.borderStyle = .roundedRect
+        view.addTarget(self, action: #selector(usernameDidChange), for: .editingChanged)
+        return view
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let view = UITextField()
+        view.placeholder = "Password"
+        view.borderStyle = .roundedRect
+        view.addTarget(self, action: #selector(passwordDidChange), for: .editingChanged)
+        return view
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let view = UIButton(type: .system)
+        view.setTitle("Login", for: .normal)
+        view.addTarget(self, action: #selector(loginButtonClickedHandler), for: .touchUpInside)
+        return view
+    }()
+    
+    private let loadingView: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    private let tipsView: UILabel = {
+        let view = UILabel()
+        view.text = "(Username is 111, password is 222)"
+        view.textColor = .lightGray
+        view.font = view.font.withSize(12)
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
-        setupSubviews()
+        self.addSubview(usernameTextField)
+        self.addSubview(passwordTextField)
+        self.addSubview(loginButton)
+        self.addSubview(loadingView)
+        self.addSubview(tipsView)
     }
     
     required init?(coder: NSCoder) {
@@ -40,32 +71,6 @@ class LoginView: BaseView<LoginViewInputEvent, LoginViewOutputEvent> {
         loginButton.center = CGPoint(x: passwordTextField.center.x, y: passwordTextField.frame.maxY + 20)
         loadingView.center = loginButton.center
         tipsView.frame = CGRect(x: 20, y: loginButton.frame.maxY + 20, width: self.frame.size.width - 40, height: 44)
-    }
-    
-    private func setupSubviews() {
-        usernameTextField.placeholder = "Username"
-        usernameTextField.borderStyle = .roundedRect
-        usernameTextField.addTarget(self, action: #selector(usernameDidChange), for: .editingChanged)
-        
-        passwordTextField.placeholder = "Password"
-        passwordTextField.borderStyle = .roundedRect
-        passwordTextField.addTarget(self, action: #selector(passwordDidChange), for: .editingChanged)
-        
-        loginButton.setTitle("Login", for: .normal)
-        
-        tipsView.text = "(Username is 111, password is 222)"
-        tipsView.textColor = .lightGray
-        tipsView.font = tipsView.font.withSize(12)
-        
-        // bind
-        loginButton.addTarget(self, action: #selector(loginButtonClickedHandler), for: .touchUpInside)
-        
-        // add subviews
-        self.addSubview(usernameTextField)
-        self.addSubview(passwordTextField)
-        self.addSubview(loginButton)
-        self.addSubview(loadingView)
-        self.addSubview(tipsView)
     }
     
     @objc private func loginButtonClickedHandler() {
